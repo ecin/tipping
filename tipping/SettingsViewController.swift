ac1f825cb430a9b3469992f8dec8f245efbe5baa
ecin <ecin@copypastel.com>
2014-08-23 06:37:06 -0700

    Initial commit.
    HUGE spike. No small commits with intermediate progress? For shame!

---
+++ tipping/SettingsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SettingsViewController.swift
+//  tipping
+//
+//  Created by Nelson Crespo on 8/23/14.
+//  Copyright (c) 2014 Copypastel. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet var doneButton : UIButton
+    @IBOutlet var defaultTipPercentageSegments : UISegmentedControl
+    
+    var settings : NSUserDefaults
+    let key = "default_tip_percentage_index"
+    
+    init(coder: NSCoder?) {
+        println("Settings init")
+        settings = NSUserDefaults.standardUserDefaults()
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        println("Settings load")
+        super.viewDidLoad()
+        
+        var defaultTipPercentageIndex = settings.integerForKey(key)
+        defaultTipPercentageSegments.selectedSegmentIndex = defaultTipPercentageIndex
+        
+        onPercentageChanged(self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func dismissModal(sender : AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func saveDefaultTipPercentage(sender : AnyObject) {
+        
+    }
+    
+    @IBAction func onPercentageChanged(sender : AnyObject) {
+        settings.setInteger(defaultTipPercentageSegments.selectedSegmentIndex, forKey: key)
+    }
+}
---
+++ tipping/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  tipping
+//
+//  Created by Nelson Crespo on 8/14/14.
+//  Copyright (c) 2014 Copypastel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var totalLabel : UILabel
+    @IBOutlet var tipLabel : UILabel
+    @IBOutlet var billField : UITextField
+    @IBOutlet var tipSelector : UISegmentedControl
+    @IBOutlet var tipAmount : UILabel
+    
+    let tipPercentages = [0.18, 0.20, 0.25]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Tipping"
+        
+        // Hide the blinking input cursor
+        billField.tintColor = UIColor.clearColor()
+        
+        // Set default settings
+        var settings = NSUserDefaults.standardUserDefaults()
+        var tipIndex = settings.integerForKey("default_tip_percentage_index")
+        tipSelector.selectedSegmentIndex = tipIndex
+        
+        // Initialize all values on screen
+        self.onEditingChanged(self)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+       var settings = NSUserDefaults.standardUserDefaults()
+        var tipIndex = settings.integerForKey("default_tip_percentage_index")
+        tipSelector.selectedSegmentIndex = tipIndex
+        
+        self.onEditingChanged(self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onEditingChanged(sender : AnyObject) {
+        var billAmount = billField.text.bridgeToObjectiveC().doubleValue
+        //var tipPercentage
+        var tip = billAmount * tipPercentages[tipSelector.selectedSegmentIndex]
+        var total = billAmount + tip
+        
+        tipAmount.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func onTap(sender : AnyObject) {
+        view.endEditing(true)
+    }
+}
+
